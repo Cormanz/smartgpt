678195affbe1c0093645df4cdb4a57d240721a9e
George Adams <george.adamopoulos13@gmail.com>
2023-05-07 16:28:27 -0400

    Add comments and improve readability in ChatGPT implementation

--- src/llm/chatgpt.rs
+++ src/llm/chatgpt.rs
@@ -1,3 +1,4 @@
+// Import the required libraries and modules
 use std::error::Error;
 
 use async_openai::{Client, types::{CreateChatCompletionResponse, CreateChatCompletionRequest, ChatCompletionRequestMessage, Role, CreateEmbeddingRequest, EmbeddingInput}, error::OpenAIError, Chat};
@@ -8,14 +9,17 @@ use tiktoken_rs::async_openai::get_chat_completion_max_tokens;
 
 use crate::{LLMProvider, Message, LLMModel};
 
+// Define the ChatGPT struct
 pub struct ChatGPT {
     pub model: String,
     pub embedding_model: String,
     pub client: Client
 }
 
+// Implement LLMModel trait for ChatGPT
 #[async_trait]
 impl LLMModel for ChatGPT {
+    // Get a response from the ChatGPT model
     async fn get_response(&self, messages: &[Message], max_tokens: Option<u16>, temperature: Option<f32>) -> Result<String, Box<dyn Error>> {
         let mut request = CreateChatCompletionRequest::default();
 
@@ -26,7 +30,6 @@ impl LLMModel for ChatGPT {
             .collect::<Vec<_>>();
 
         request.temperature = temperature;
-
         request.max_tokens = max_tokens;
         
         let response: CreateChatCompletionResponse = self.client
@@ -37,6 +40,7 @@ impl LLMModel for ChatGPT {
         Ok(response.choices[0].message.content.clone())
     }
     
+    // Get base embed for a given text
     async fn get_base_embed(&self, text: &str) -> Result<Vec<f32>, Box<dyn Error>> {
         let embeddings = self.client.embeddings().create(CreateEmbeddingRequest {
             model: self.embedding_model.clone(),
@@ -47,6 +51,7 @@ impl LLMModel for ChatGPT {
         Ok(embeddings.data[0].embedding.clone())
     }
 
+    // Get the number of tokens remaining
     fn get_tokens_remaining(&self, messages: &[Message]) -> Result<usize, Box<dyn Error>> {
         let messages: Vec<ChatCompletionRequestMessage> = messages
             .iter()
@@ -58,6 +63,7 @@ impl LLMModel for ChatGPT {
     }
 }
 
+// Define the ChatGPTConfig struct
 #[derive(Serialize, Deserialize)]
 pub struct ChatGPTConfig {    
     #[serde(rename = "api key")] pub api_key: String,
@@ -65,8 +71,10 @@ pub struct ChatGPTConfig {
     #[serde(rename = "embedding model")] pub embedding_model: Option<String>,
 }
 
+// Define the ChatGPTProvider struct
 pub struct ChatGPTProvider;
 
+// Implement LLMProvider trait for ChatGPTProvider
 #[async_trait]
 impl LLMProvider for ChatGPTProvider {
     fn is_enabled(&self) -> bool {
@@ -88,10 +96,12 @@ impl LLMProvider for ChatGPTProvider {
     }
 }
 
+// Function to create a ChatGPT model
 pub fn create_model_chatgpt() -> Box<dyn LLMProvider> {
     Box::new(ChatGPTProvider)
 }
 
+// Implement the From trait to convert a Message into a ChatCompletionRequestMessage
 impl From<Message> for ChatCompletionRequestMessage {
     fn from(value: Message) -> Self {
         match value {
