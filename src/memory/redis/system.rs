db03404a5064bcf9ce3fc1f8348f748d06b58309
Phil C <pcrilley06@gmail.com>
2023-05-16 23:55:52 -0400

    Refactor redis memory system into multiple files

--- src/memory/redis/system.rs
+++ src/memory/redis/system.rs
@@ -6,15 +6,11 @@ use serde::{Deserialize, Serialize};
 use tokio::runtime::Runtime;
 use redis::Value::*;
 
-use crate::{LLM, Memory, MemoryProvider, RelevantMemory};
+use crate::{LLM, Memory, MemoryProvider, RelevantMemory, MemorySystem, set_json_record, search_vector_field, create_index_if_not_exists};
 
 use tokio::{sync::Mutex};
 
-use super::MemorySystem;
-
 use async_trait::async_trait;
-
-use std::borrow::Borrow;
 
 #[derive(Serialize, Deserialize)]
 pub struct EmbeddedMemory {
@@ -155,106 +151,6 @@ impl MemoryProvider for RedisProvider {
             index_name: index_name.to_string(), // This could be configured differently depending on your needs
         }))
     }
-}
-
-async fn execute_redis_command<T: redis::FromRedisValue, S: Borrow<str>>(
-    con: &mut redis::aio::Connection,
-    command: &str,
-    args: &[S],
-) -> redis::RedisResult<T> {
-    let mut cmd = redis::cmd(command);
-    for arg in args {
-        cmd.arg(arg.borrow());
-    }
-    cmd.query_async(con).await
-}
-
-async fn create_index_if_not_exists(con: &mut redis::aio::Connection, index_name: &str, field_path: &str, dimension: usize) -> redis::RedisResult<()> {
-    let index_exists: bool = redis::cmd("FT.INFO")
-        .arg(index_name)
-        .query_async(con)
-        .await
-        .map(|_: redis::Value| true)
-        .or_else(|err: redis::RedisError| {
-            if err.kind() == redis::ErrorKind::TypeError {
-                Ok(false)
-            } else {
-                Err(err)
-            }
-        })?;
-
-    if !index_exists {
-        let _ = execute_redis_command::<redis::Value, _>(
-            con,
-            "FT.CREATE",
-            &[
-                index_name,
-                "ON",
-                "JSON",
-                "SCHEMA",
-                field_path,
-                "as",
-                "vector",
-                "VECTOR",
-                "FLAT",
-                "6",
-                "TYPE",
-                "FLOAT32",
-                "DIM",
-                &dimension.to_string(),
-                "DISTANCE_METRIC",
-                "L2"
-            ],
-        ).await;
-    }
-
-    Ok(())
-}
-
-async fn search_vector_field(
-    con: &mut redis::aio::Connection,
-    index_name: &str,
-    query_blob: &[u8],
-    k: usize,
-) -> redis::RedisResult<redis::Value> {
-    // check if k can be formatted as a string to prevent panic
-    let k_str = k.to_string();
-    if k_str.is_empty() {
-        return Err(redis::RedisError::from((redis::ErrorKind::TypeError, "Invalid k value")));
-    }
-
-    let query_blob_str = base64::encode(query_blob);
-
-    execute_redis_command::<redis::Value, _>(
-        con,
-        "FT.SEARCH",
-        &[
-            index_name,
-            &format!("*=>[KNN {} @vec $BLOB]", k_str),
-            "PARAMS",
-            "2",
-            "BLOB",
-            &query_blob_str,
-            "DIALECT",
-            "2",
-        ],
-    ).await
-}
-
-async fn set_json_record(
-    con: &mut redis::aio::Connection,
-    point_id: &str,
-    embedded_memory: &EmbeddedMemory,
-) -> redis::RedisResult<()> {
-    execute_redis_command::<(), &str>(
-        con,
-        "JSON.SET",
-        &[
-            point_id,
-            "$",
-            &serde_json::to_value(&embedded_memory)?.to_string(),
-        ],
-    ).await
 }
 
 pub fn create_memory_redis() -> Box<dyn MemoryProvider> {
---
+++ src/memory/redis/utils.rs
@@ -0,0 +1,103 @@
+use crate::{EmbeddedMemory};
+
+use std::borrow::Borrow;
+
+pub async fn execute_redis_command<T: redis::FromRedisValue, S: Borrow<str>>(
+    con: &mut redis::aio::Connection,
+    command: &str,
+    args: &[S],
+) -> redis::RedisResult<T> {
+    let mut cmd = redis::cmd(command);
+    for arg in args {
+        cmd.arg(arg.borrow());
+    }
+    cmd.query_async(con).await
+}
+
+pub async fn create_index_if_not_exists(con: &mut redis::aio::Connection, index_name: &str, field_path: &str, dimension: usize) -> redis::RedisResult<()> {
+    let index_exists: bool = redis::cmd("FT.INFO")
+        .arg(index_name)
+        .query_async(con)
+        .await
+        .map(|_: redis::Value| true)
+        .or_else(|err: redis::RedisError| {
+            if err.kind() == redis::ErrorKind::TypeError {
+                Ok(false)
+            } else {
+                Err(err)
+            }
+        })?;
+
+    if !index_exists {
+        let _ = execute_redis_command::<redis::Value, _>(
+            con,
+            "FT.CREATE",
+            &[
+                index_name,
+                "ON",
+                "JSON",
+                "SCHEMA",
+                field_path,
+                "as",
+                "vector",
+                "VECTOR",
+                "FLAT",
+                "6",
+                "TYPE",
+                "FLOAT32",
+                "DIM",
+                &dimension.to_string(),
+                "DISTANCE_METRIC",
+                "L2"
+            ],
+        ).await;
+    }
+
+    Ok(())
+}
+
+pub async fn search_vector_field(
+    con: &mut redis::aio::Connection,
+    index_name: &str,
+    query_blob: &[u8],
+    k: usize,
+) -> redis::RedisResult<redis::Value> {
+    // check if k can be formatted as a string to prevent panic
+    let k_str = k.to_string();
+    if k_str.is_empty() {
+        return Err(redis::RedisError::from((redis::ErrorKind::TypeError, "Invalid k value")));
+    }
+
+    let query_blob_str = base64::encode(query_blob);
+
+    execute_redis_command::<redis::Value, _>(
+        con,
+        "FT.SEARCH",
+        &[
+            index_name,
+            &format!("*=>[KNN {} @vec $BLOB]", k_str),
+            "PARAMS",
+            "2",
+            "BLOB",
+            &query_blob_str,
+            "DIALECT",
+            "2",
+        ],
+    ).await
+}
+
+pub async fn set_json_record(
+    con: &mut redis::aio::Connection,
+    point_id: &str,
+    embedded_memory: &EmbeddedMemory,
+) -> redis::RedisResult<()> {
+    execute_redis_command::<(), &str>(
+        con,
+        "JSON.SET",
+        &[
+            point_id,
+            "$",
+            &serde_json::to_value(&embedded_memory)?.to_string(),
+        ],
+    ).await
+}
